c5d2c8b623f1bf0ee2e554cf7679933731291b87
sxs6596 <sxs6596@mavs.uta.edu>
2023-08-31 15:27:10 -0500

    add replace with pi

---
+++ recursion/replaceWithPi.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+void replaceWithPi(char str[], int start)
+{
+    // base case.
+    if (str[start] == '\0' or str[start + 1] == '\0')
+    {
+        return;
+    }
+    // recursive call.
+    replaceWithPi(str, start + 1);
+
+    // small work.
+    if (str[start] == 'p' and str[start + 1] == 'i')
+    {
+        for (int i = strlen(str); i >= start + 2; i--)
+        {
+            str[i + 2] = str[i];
+        }
+        str[start] = '3';
+        str[start + 1] = '.';
+        str[start + 2] = '1';
+        str[start + 3] = '4';
+    }
+}
+int main()
+{
+    char str[] = "pippppiiiipi";
+    replaceWithPi(str, 0);
+    cout << str << endl;
+    return 0;
+}
