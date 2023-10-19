741dba3b25366850df65480cf525afaa6bacf871
sxs6596 <127999061+sxs6596@users.noreply.github.com>
2023-10-19 12:13:46 -0500

    Time: 7 ms (90.03%), Space: 11.2 MB (23.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // define the size. 
         int n = nums.size();
+        // define the hashmap. 
+        unordered_map<int,int>mp;
+        
+        // define the ans vector 
         vector<int>ans; 
-        for(int i = 0;i<n-1;i++){
-            for(int j = i+1;j<n;j++){
-                if(nums[i] + nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
+        for(int i = 0;i<n;i++){
+            int need = target - nums[i];
+            auto it = mp.find(need);
+            if(it != mp.end()){
+                ans.push_back(it->second);
+                ans.push_back(i);
+                return ans; 
+            }else{
+                mp[nums[i]] = i; 
             }
         }
         return ans;
