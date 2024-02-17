ac06ea891f8b2326d2c973439956f4e69bccd532
Siva Sri Harsha Suthapalli <127999061+sxs6596@users.noreply.github.com>
2024-02-17 16:42:36 -0600

    Time: 4 ms (93.75%), Space: 21.5 MB (44.81%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,28 +11,30 @@
  */
 class Solution {
 public:
-   
     bool isBalanced(TreeNode* root) {
-      if(dfsHeight(root)!=-1){
-          return true;
-      }else{
-          return false;
-      }
-        
-    }
-    int dfsHeight(TreeNode* root){ 
-     // base case. 
-        if(root == nullptr){
-            return 0;
+        // return true if balanced 
+          if(check(root)!=-1){
+              return true;
+          } 
+        // return false if not balanced 
+        else{
+            return false; 
         }
-        int lh = dfsHeight(root->left);
-        if(lh == -1) return -1;
-        int rh = dfsHeight(root->right);
+    }
+    
+    int check(TreeNode* root){
+        // base case 
+        if(root == nullptr){
+            return 0; 
+        }
+        int lh = check(root->left);
+        if(lh == -1) return -1; 
+        int rh = check(root->right);
         if(rh == -1) return -1; 
-        if(abs(lh-rh)>1 ){
-            return -1;
-        }else{
-           return 1+max(lh, rh);    
+        // check the absolute difference 
+        if(abs(lh - rh)>1) return -1; 
+        else{
+            return 1+max(lh,rh); 
         }
     }
 };
