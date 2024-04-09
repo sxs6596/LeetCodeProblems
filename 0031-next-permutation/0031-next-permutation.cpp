bf7784dc8e53338aef482e165dfdd218b68294b0
Siva Sri Harsha Suthapalli <127999061+sxs6596@users.noreply.github.com>
2024-04-09 16:12:22 -0500

    Time: 0 ms (100.00%), Space: 14.6 MB (18.56%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,27 +1,29 @@
+// Longest Prefix
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        // define the size
-        int n = nums.size();
-        // define the index; 
-        int index = -1;
-        // longest prefix case 
-        for(int i= n-2; i>=0; i--){
+//       find the index or the break point 
+        int n = nums.size(); 
+        int index = -1; 
+        for(int i = n-2 ;i>=0;i--){
             if(nums[i] < nums[i+1]){
-                index = i;
-                break;
+                index = i; 
+                break; 
             }
         }
+//         no permuation case 
         if(index == -1){
             reverse(nums.begin(), nums.end());
             return; 
         }
-        for(int i = n-1;i>index;i--){
+//         swap with the smallest index after 
+        for(int i= n-1;i>=0;i--){
             if(nums[i] > nums[index]){
                 swap(nums[i], nums[index]);
-                break;
+                break; 
             }
         }
-        reverse(nums.begin()+index+1, nums.end());
+//         sort from the index + 1 position to the end.
+        sort(nums.begin()+index+1, nums.end()); 
     }
 };
