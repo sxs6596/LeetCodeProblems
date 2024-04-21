93a9f9cdf20d2d0ee56899a114bed4601daa88c4
Siva Sri Harsha Suthapalli <127999061+sxs6596@users.noreply.github.com>
2024-04-21 16:12:33 -0500

    Time: 12 ms (61.13%), Space: 14 MB (29.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // define the size. 
-        int n = nums.size();
-        // define the hashmap. 
-        unordered_map<int,int>mp;
-        
-        // define the ans vector 
-        vector<int>ans; 
-        for(int i = 0;i<n;i++){
-            int need = target - nums[i];
-            auto it = mp.find(need);
-            if(it != mp.end()){
-                ans.push_back(it->second);
-                ans.push_back(i);
-                return ans; 
-            }else{
-                mp[nums[i]] = i; 
+        unordered_map<int,int>mp; 
+        for(int i =0;i<nums.size();i++){
+            int difference = target-nums[i];
+            if(mp.find(difference)!=mp.end()){
+                return {mp[difference], i}; 
             }
+            mp[nums[i]] = i; 
         }
-        return ans;
+        return {};
     }
 };
