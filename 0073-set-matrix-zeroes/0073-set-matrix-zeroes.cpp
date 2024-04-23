7b2fe9d6140c7b928c8599bd14bc104c968c1482
Siva Sri Harsha Suthapalli <127999061+sxs6596@users.noreply.github.com>
2024-04-23 17:03:17 -0500

    Time: 7 ms (83.59%), Space: 16.3 MB (24.83%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-//        assume m rows and n columns
+//         m x n matrix 
         int m = matrix.size(); 
         int n = matrix[0].size(); 
-        vector<int>row(m,0);
-        vector<int>col(n,0); 
-        
-        for(int i = 0;i<m;i++){
+//         define the vectors to store rows and cols which are zeros 
+        vector<int>rows(m,0);
+        vector<int>cols(n,0);
+        for(int i =0;i<m;i++){
             for(int j =0;j<n;j++){
                 if(matrix[i][j] == 0){
-                    row[i] = 1; 
-                    col[j] = 1; 
+                    rows[i] = 1; 
+                    cols[j] = 1; 
                 }
             }
         }
         for(int i =0;i<m;i++){
             for(int j =0;j<n;j++){
-                if(row[i] || col[j]){
-                    matrix[i][j] = 0;
+                if(rows[i] || cols[j]){
+                    matrix[i][j] = 0; 
                 }
             }
         }
