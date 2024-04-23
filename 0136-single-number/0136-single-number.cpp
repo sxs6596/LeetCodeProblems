2955bd9385d9dfa73b0564136b10c6ad4768fa87
Siva Sri Harsha Suthapalli <127999061+sxs6596@users.noreply.github.com>
2024-04-22 20:18:20 -0500

    Time: 11 ms (82.85%), Space: 19.2 MB (85.39%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if(nums.size() ==0)return 0; 
-        sort(nums.begin(), nums.end());
-        stack<int>st; 
-        
-        for(int i =0;i<nums.size();i++){
-            if(st.empty() || st.top() != nums[i]){
-                st.push(nums[i]);
-            }else{
-                if(!st.empty()){
-                    st.pop(); 
-                }
-            }
+        int val = 0 ; 
+        int n = nums.size(); 
+        for(int i =0;i<n;i++){
+            val = val^nums[i];
         }
-        if(!st.empty()){
-            return st.top(); 
-        }else{
-            return 0; 
-        }
+        return val;
     }
 };
